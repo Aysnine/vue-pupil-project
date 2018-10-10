1a79ff97ede371de7ad074af3d90228d5c1beb0c
CNine <cnine229@gmail.com>
2018-10-10 13:42:02 +0800

    use modern-normalize element-ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'modern-normalize/modern-normalize.css'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/style/index.styl'
+
+import locale from 'element-ui/lib/locale/lang/zh-CN'
+import ElementUI from 'element-ui'
+
+Vue.use(ElementUI, { /* size: 'small', */ locale })
 Vue.config.productionTip = false
 
 new Vue({
