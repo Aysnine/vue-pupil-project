b1191da6a7e9b5b776126951ca218319c358e6bc
CNine <cnine229@gmail.com>
2018-10-10 11:07:01 +0800

    change router construct

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './pages/Home.vue'
+import Home from '@/pages/Home.vue'
 
 Vue.use(Router)
 
@@ -20,7 +20,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ './pages/About.vue')
+        import(/* webpackChunkName: "about" */ '@/pages/About.vue')
     }
   ]
 })
