b090f3a7d2b3efc257c9e92220da07987d541990
CNine <cnine229@gmail.com>
2018-10-12 20:24:57 +0800

    add splash/login page

---
+++ src/pages/splash/login/index.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .wrap-page
+    el-tooltip(effect='dark', content='来盗我QQ啊', placement='left')
+      img.logo-image(alt='Pupil logo', src='@/assets/logo.png')
+    login-box(style='margin: auto', message='hello')
+      pretty-tips(slot='prepend', align='center', message='账号 admin，密码 admin')
+</template>
+
+<style lang="stylus" scoped>
+.wrap-page
+  margin 0 auto
+  text-align center
+  .logo-image
+    height 130px
+    margin 1em
+</style>
