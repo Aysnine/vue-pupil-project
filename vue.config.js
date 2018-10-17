246c7bcac5a39025af7e1930bebb3ff5a8a52de2
CNine <cnine229@gmail.com>
2018-10-17 10:32:43 +0800

    add nib of stylus

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  productionSourceMap: false // ! 避免 build 后在浏览器控制台 Resource 中暴露源码
-  // devServer: {
-  //   proxy: ''
-  // }
+  productionSourceMap: false, // ! 避免 build 后在浏览器控制台 Resource 中暴露源码
+  css: {
+    loaderOptions: {
+      // http://stylus.github.io/nib/
+      stylus: {
+        use: [require('nib')()],
+        import: ['~nib/lib/nib/index.styl']
+      }
+    }
+  }
 }
