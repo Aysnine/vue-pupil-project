ef7dfa4557ba340d93a4a12b4b82d4120c92d71d
CNine <cnine229@gmail.com>
2018-10-31 08:51:46 +0800

    改进 404 页面

--- src/pages/404/index.vue
+++ src/pages/404/index.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
   .notfound-wrap
     p.title 404
-    img.fun(src='/images/fun/0')
     p.desc 页面不存在
+    img.fun(src='/images/fun/0')
     p.desc
       router-link(to='/')
-        el-button(type='primary', round='', icon='el-icon-location-outline') 返回首页
+        el-button(type='primary', round, icon='el-icon-location-outline') 返回首页
+      el-button(type='primary', plain, round, icon='el-icon-arrow-left', @click='$router.go(-1)') 上一页
 </template>
 
 <style lang="stylus" scoped>
@@ -19,4 +20,6 @@
     font-size 4em
     font-weight bold
     margin 10px 0
+.desc > *
+  margin 1em
 </style>
