ed1f5af94f02b34a2ad1f91ec01883b1f320d15a
CNine <cnine229@gmail.com>
2018-10-31 08:51:15 +0800

    添加 quill 富文本组件

--- src/components/index.js
+++ src/components/index.js
@@ -6,8 +6,10 @@ import LoginBox from './LoginBox'
 import PrettyTips from './PrettyTips'
 import IdentifyCode from './IdentifyCode'
 import PureNavMenu from './PureNavMenu'
+import QuillEditor from './QuillEditor'
 
 Vue.component(LoginBox.name, LoginBox)
 Vue.component(PrettyTips.name, PrettyTips)
 Vue.component(IdentifyCode.name, IdentifyCode)
 Vue.component(PureNavMenu.name, PureNavMenu)
+Vue.component(QuillEditor.name, QuillEditor)
