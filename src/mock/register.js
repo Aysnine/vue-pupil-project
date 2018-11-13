be1393f0d8fa327608562852a4d9a4748ea1b2fc
CNine <cnine229@gmail.com>
2018-11-13 14:57:06 +0800

    添加 mock 扩展：shortid

--- src/mock/register.js
+++ src/mock/register.js
@@ -1,11 +1,16 @@
 import MockSuit from '@/lib/main/mock-suit'
 import db from './db.js'
+import shortid from 'shortid'
+shortid.characters(
+  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_$'
+)
 
 import app from '@/mock/app'
 import dash from '@/mock/admin/dash'
 import task from '@/mock/admin/task'
 import summary from '@/mock/admin/summary'
 
+MockSuit.extend('uid', shortid.generate)
 MockSuit.extend('db', db)
 
 MockSuit.load(app)
